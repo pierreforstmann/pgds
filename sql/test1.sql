90395750efe231923c5f893b5062a0faa251393b
Pierre <pierre.forstmann@free.fr>
2023-08-06 14:14:25 +0200

    added sql/test1.sql + expected/test1.out + pgds.conf

---
+++ sql/test1.sql
@@ -0,0 +1,15 @@
+--
+-- test1.sql
+create extension pgds;
+--
+create table t1(x int);
+create table t2(x int);
+--
+select * from t1;
+select * from t1;
+--
+insert into t2 values(2);
+select * from t2;
+select * from t2;
+--
+select * from t1;
