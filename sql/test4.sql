0139f6c5749c8455d44cbf7ee6c2b63fc06b1e76
Pierre <pierre.forstmann@free.fr>
2023-08-21 18:22:06 +0200

    test4: added test case with scalar subqueries in target list

--- sql/test4.sql
+++ sql/test4.sql
@@ -6,8 +6,16 @@ create table t42(x2 int);
 --
 select * from t41 where x1 = (select max(x2) from t42);
 --
+--
 create table t43(x3 int);
 create table t44(x4 int);
-
+--
 with v as (select x3 from t43) select count(*) from v where x3 = 123;
+--
+--
+select 
+ (select min(x3) from t43) as c1, 
+ (select max(x4) from t44) as c2, 
+ avg(x1) as c3 from t41;
+
 
