31a1950f8b39246d852a606e1b583fba491639c9
Pierre <pierre.forstmann@free.fr>
2023-08-21 18:13:09 +0200

    test4: added CTE query

--- sql/test4.sql
+++ sql/test4.sql
@@ -1,7 +1,13 @@
 --
--- test1.sql
+-- test5.sql
 --
 create table t41(x1 int);
 create table t42(x2 int);
 --
 select * from t41 where x1 = (select max(x2) from t42);
+--
+create table t43(x3 int);
+create table t44(x4 int);
+
+with v as (select x3 from t43) select count(*) from v where x3 = 123;
+
